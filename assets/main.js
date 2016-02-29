b0a352abe54e42a49ca1a826a049db9616b9ba55
Rebekah <rebekah.adair@yahoo.com>
2016-02-29 11:29:24 -0500

    para para paradise

--- assets/main.js
+++ assets/main.js
@@ -12,18 +12,3 @@
     var x = Math.ceil(numWords / 230);
     timeToRead.innerHTML = x + ' min read';
 })();
-
-// loop or reduce to find totalWords in para which is now an array because .slice.call
-//  use text content to get actual content not just number of para objects
-
-
-
-
-
-// (function readTime () {
-//     var para = document.querySelectorAll('.para');
-//     var timeToRead = document.querySelector('.timeToRead');
-//     var words = para.innerHTML.split(' ');
-//     var x = Math.ceil(totalWords / 230);
-//     timeToRead.innerHTML = x + ' min read' + '(' + totalWords;
-// })();
