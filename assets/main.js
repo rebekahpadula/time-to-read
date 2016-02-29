913c66510d111be6da434dda4348e589a07248c6
Rebekah <rebekah.adair@yahoo.com>
2016-02-29 11:51:21 -0500

    reduce

--- assets/main.js
+++ assets/main.js
@@ -5,9 +5,14 @@
     var timeToRead = document.querySelector('.timeToRead');
     var numWords = 0;
 
-    for (var i = 0; i < para.length; i++) {
-        numWords += para[i].textContent.split(' ').length;
-    };
+    numWords = para.reduce(function (a, b) {
+        return a + b.textContent.split(' ').length;
+    }, 0);
+    
+    // or instead of reduce you could use a loop
+    // for (var i = 0; i < para.length; i++) {
+    //     numWords += para[i].textContent.split(' ').length;
+    // };
 
     var x = Math.ceil(numWords / 230);
     timeToRead.innerHTML = x + ' min read';
